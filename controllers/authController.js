b767d6334c09469bd00e2b4994f00d486842cb0b
Md Atikuzzaman <akash203037@gmail.com>
2025-01-17 16:01:29 +0600

    users get api created

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,3 +33,15 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+exports.users = async(req, res)=>{
+    try{
+        const users = await User.find()
+        if(!users){
+            res.status(404).json({message: "users not found"})
+        }
+        res.status(200).json(users)
+    }catch(err){
+        res.status(500).json({message: "server error", err})
+    }
+}
