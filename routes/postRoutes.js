0060ddfe1f1800e8cd116d66bbc4e7908ebc35dc
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-01-22 11:46:22 +0600

    vote count and view count api created

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const { createPost, getPosts, getPostDetails } = require('../controllers/postController');
+const express = require("express");
+const {
+  createPost,
+  getPosts,
+  getPostDetails,
+  updatePostViews,
+  updatePostVotes,
+} = require("../controllers/postController");
 const router = express.Router();
 
-router.post('/', createPost);
-router.get('/', getPosts);
-router.get('/:id',getPostDetails)
-module.exports = router;
+router.post("/", createPost);
+router.get("/", getPosts);
+router.get("/:id", getPostDetails);
 
+router.patch("/views", updatePostViews); // Route to update view count
+router.patch("/votes", updatePostVotes); // Route to update vote count
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 dotenv.config();
 connectDB();
@@ -10,16 +10,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 // Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/posts', require('./routes/postRoutes'));
-app.use('/api/communities', require('./routes/communityRoutes'));
-app.use('/api/comments', require('./routes/commentRoutes'));
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/posts", require("./routes/postRoutes"));
+app.use("/api/communities", require("./routes/communityRoutes"));
+app.use("/api/comments", require("./routes/commentRoutes"));
 
-app.get("/", async(req, res)=>{
-    res.send("hello world")
-})
+app.get("/", async (req, res) => {
+  res.send("hello world");
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
