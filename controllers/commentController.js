c582c18eb5a69acc4e487bb0ddaec65c8df7b1a5
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-28 02:28:03 +0600

    aaded

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -81,4 +81,47 @@ const getCommentsByPost = async (req, res) => {
   }
 };
 
-module.exports = { addCommentOrReply, getCommentsByPost };
+const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find()
+      .populate("postId", "title") 
+      .populate("parentComment", "content") // Populate parent comment (e.g., content)
+      .exec();
+
+    res.status(200).json(comments);
+  } catch (err) {
+    console.error("Error fetching comments:", err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Update a comment by ID
+const updateComment = async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      commentId,
+      { content },
+      { new: true }
+    );
+
+    if (!updatedComment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+
+    res.status(200).json({
+      message: "Comment updated successfully",
+      comment: updatedComment,
+    });
+  } catch (error) {
+    console.error("Error updating comment:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Delete comment
+
+
+module.exports = { addCommentOrReply, getCommentsByPost, getAllComments,updateComment };
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   addCommentOrReply,
   getCommentsByPost,
+  getAllComments,
 } = require("../controllers/commentController");
 
 const router = express.Router();
@@ -9,9 +10,12 @@ const router = express.Router();
 // Add a comment or reply
 router.post("/", addCommentOrReply);
 
+// get all comments
+router.get("/all", getAllComments);
+
 // Get all comments and nested replies for a specific post
 router.get("/:postId", getCommentsByPost);
 
 
 
-module.exports = router; 
+module.exports = router;
