39cef880ebaac6fbce611646feae2dbbb496c8b7
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-31 05:00:27 +0600

    added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { register, login, users, user } = require('../controllers/authController');
+const { register, login, users, user, getUserByEmail } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
 router.get("/users", users)
 router.get("/user/:email", user)
+
+// admin side code
+// Endpoint to get user by email
+router.get('/user/:email', getUserByEmail);
+
 
 module.exports = router;
